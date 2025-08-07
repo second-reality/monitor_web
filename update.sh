7f573b218cf6400253df28d7eb78c96182d610e7
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2025-08-07 10:56:07 -0700

    update

--- update.sh
+++ update.sh
@@ -24,9 +24,9 @@ rm -f credentials.txt config.ovpn
 echo
 curl https://ipinfo.io/ip
 echo
-su user -c \
-'xvfb-run cutycapt --url=https://gointernational.ca/programs/canada-working-holiday-program-sponsored/ --user-agent="Mozilla/5.0 (Windows NT 11.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.6998.166 Safari/537.36" --out=/tmp/page.png --delay=10000'
-mv /tmp/page.png page.png
+#su user -c \
+#'xvfb-run cutycapt --url=https://gointernational.ca/programs/canada-working-holiday-program-sponsored/ --user-agent="Mozilla/5.0 (Windows NT 11.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.6998.166 Safari/537.36" --out=/tmp/page.png --delay=10000'
+#mv /tmp/page.png page.png
 elinks https://gointernational.ca/programs/canada-working-holiday-program-sponsored/ -dump > page.txt
 
 pkill openvpn
